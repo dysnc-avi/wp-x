bc59503b8d0e85d93e5004a238b61ec70c0f3b59
Abhinav <abhinav.contact22@gmail.com>
2025-02-05 23:36:25 +0530

    Higher order functions for ease of use

---
+++ server/src/handlers/asyncHandler.js
@@ -0,0 +1,10 @@
+const asyncHandler = (fn) => async (req, res, next) => {
+  try {
+    fn(req, res, next);
+  } catch (error) {
+    res.json({
+      status: 500,
+      message: "Internal server Error",
+    });
+  }
+};
