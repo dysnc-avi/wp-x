ccedf246615cc92728769f7ae41ac3fb0b07590b
Abhinav <abhinav.contact22@gmail.com>
2025-02-05 23:51:28 +0530

    dotenv config

--- server/src/app.js
+++ server/src/app.js
@@ -1,9 +1,11 @@
 import express from "express";
 import userRouter from "./routes/user.router.js";
 import cookieParser from "cookie-parser";
+import dotenv from "dotenv";
 
 const app = express();
 
+dotenv.config();
 app.use(express.json({ limit: "16kb" }));
 app.use(cookieParser());
 
