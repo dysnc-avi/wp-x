878e28310a7cb73ffe7c60e2183a290b18fa696d
Abhinav <abhinav.contact22@gmail.com>
2025-02-10 14:28:36 +0530

    Removed unwanted files

--- server/src/controllers/user.entry.js
+++
@@ -1,6 +0,0 @@
-import asyncHandler from "../handlers/asyncHandler.js";
-const userEntry = asyncHandler(async (req, res) => {
-  return res.json({ status: 200, message: "Working" });
-});
-
-export default userEntry;
