09b644a1af89b9ecb16495ff4ea8cc1dca775bdf
Abhinav <abhinav.contact22@gmail.com>
2025-02-11 22:29:12 +0530

    End config

--- server/src/app.js
+++ server/src/app.js
@@ -11,9 +11,11 @@ app.use(cookieParser());
 //routes
 import userRouter from "./routes/user.router.js";
 import organisationRouter from "./routes/organisation.route.js";
+import workspaceRouter from "./routes/workspace.router.js";
 
 app.use("/user", userRouter);
 app.use("/organisation", organisationRouter);
+app.use("/workspace", workspaceRouter);
 
 app.get("/", (req, res) => {
   res.json({
