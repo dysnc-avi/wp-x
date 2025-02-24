5118914e8e74e0b7db55d5dc1d146db4133a40ed
Abhinav <abhinav.contact22@gmail.com>
2025-02-24 23:10:31 +0530

    Basic structure of signup page

---
+++ client/src/components/SignUp.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+const Signup = () => {
+  return (
+    <div className="flex w-screen h-lvh">
+      <div className="bg-black w-1/3">s</div>
+      <div className="bg-white w-full"></div>
+    </div>
+  );
+};
+
+export default Signup;
