15ad1e8e9273c6070588fa321ded82cd285d0de2
Abhinav <abhinav.contact22@gmail.com>
2025-02-25 23:53:16 +0530

    Simple signup page completed without api calls

--- client/src/components/SignUp.jsx
+++ client/src/components/SignUp.jsx
@@ -15,6 +15,25 @@ const Signup = () => {
   });
   const [inputType, setInputType] = useState("password");
 
+  function checkInputs() {
+    if (user.fullName != "" && user.email != "" && user.password != "") {
+      return true;
+    } else {
+      Object.keys(user).forEach((key) => {
+        if (user[key] == "") {
+          setInputErrors((prevErrors) => ({ ...prevErrors, [key]: true }));
+        }
+      });
+    }
+  }
+
+  function handleSubmit(e) {
+    e.preventDefault();
+    if (checkInputs()) {
+      console.log(user);
+    }
+  }
+
   return (
     <div className="flex md:w-screen h-lvh font-inter">
       <div className="bg-black w-1/3">s</div>
@@ -33,7 +52,10 @@ const Signup = () => {
           </p>
         </div>
         {/* form */}
-        <form className="mx-24 mt-5 flex flex-col gap-4">
+        <form
+          className="mx-24 mt-5 flex flex-col gap-4"
+          onSubmit={(e) => handleSubmit(e)}
+        >
           <div className="w-full h-fit p-2 pl-4 bg-red-100 rounded-xl">
             <span className="text-red-600 text-sm">{errorMessage}</span>
           </div>
@@ -41,10 +63,18 @@ const Signup = () => {
             type="text"
             className="border border-black rounded-lg pl-4 py-3 placeholder:text-sm"
             placeholder="Full Legal Name"
-            onChange={(e) => setUser({ ...user, fullName: e.target.value })}
+            onChange={(e) => {
+              setUser({ ...user, fullName: e.target.value });
+              if (inputErrors.fullName == true) {
+                setInputErrors((prevErrors) => ({
+                  ...prevErrors,
+                  fullName: false,
+                }));
+              }
+            }}
           />
           {inputErrors.fullName == true ? (
-            <h1 className="text-sm pl-2 text-red-600">
+            <h1 className="text-xs pl-2 text-red-600">
               Name field is required
             </h1>
           ) : null}
@@ -52,10 +82,18 @@ const Signup = () => {
             type="email"
             className="border border-black rounded-lg pl-4 py-3 placeholder:text-sm"
             placeholder="Email (preferably Business Email)"
-            onChange={(e) => setUser({ ...user, email: e.target.value })}
+            onChange={(e) => {
+              setUser({ ...user, email: e.target.value });
+              if (inputErrors.email == true) {
+                setInputErrors((prevErrors) => ({
+                  ...prevErrors,
+                  email: false,
+                }));
+              }
+            }}
           />
           {inputErrors.email == true ? (
-            <h1 className="text-sm pl-2 text-red-600">
+            <h1 className="text-xs pl-2 text-red-600">
               Email field is required
             </h1>
           ) : null}
@@ -63,7 +101,15 @@ const Signup = () => {
             type={inputType}
             className="border border-black rounded-lg pl-4 py-3 placeholder:text-sm"
             placeholder="Password"
-            onChange={(e) => setUser({ ...user, password: e.target.value })}
+            onChange={(e) => {
+              setUser({ ...user, password: e.target.value });
+              if (inputErrors.password == true) {
+                setInputErrors((prevErrors) => ({
+                  ...prevErrors,
+                  password: false,
+                }));
+              }
+            }}
           />
           <button
             type="button"
@@ -75,11 +121,18 @@ const Signup = () => {
             {inputType === "password" ? "Show" : "Hide"} password
           </button>
           {inputErrors.email == true ? (
-            <h1 className="text-sm pl-2 text-red-600">
+            <h1 className="text-xs pl-2 text-red-600">
               Password field is required
             </h1>
           ) : null}
+          <button
+            type="submit"
+            className="self-end text-sm bg-black text-white p-2 rounded-lg cursor-pointer font-semibold"
+          >
+            Sign Up
+          </button>
         </form>
+        {/* //todo: Add passport js */}
       </div>
     </div>
   );
